8ca7521dda8b08e7d8e0799cbbf32feb398c224d
豊田大貴 <toyodahiroki@toyodadaikinoMacBook-Pro.local>
2021-05-06 04:34:03 +0900

    Initial commit

---
+++ main/java/com/example/myapp/application/form/CommentForm.java
@@ -0,0 +1,11 @@
+package com.example.myapp.application.form;
+
+import lombok.Data;
+
+@Data
+public class CommentForm {
+    private String name;
+    private String mailAddress;
+    private String comment;
+}
+
---
+++ main/java/com/example/myapp/presentation/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.myapp.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HelloController {
+    @GetMapping("/hello") // localhost:8080/helloはここにアクセスが来る
+    public String hello(Model model) {
+        return "hello"; //ここでhello.htmlを返す　
+    }
+}
