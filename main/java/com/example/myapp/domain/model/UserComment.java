318c3c05fa6bab510ccbfba5b5ae9e81d8f10228
toyodahiroki <toyo170920@gmail.com>
2021-05-14 19:13:56 +0900

    ドメインオブジェクトの作成

---
+++ main/java/com/example/myapp/domain/model/UserComment.java
@@ -0,0 +1,47 @@
+package com.example.myapp.domain.model;
+
+import com.example.myapp.domain.type.Comment;
+import com.example.myapp.domain.type.MailAddress;
+import com.example.myapp.domain.type.Name;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Random;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class UserComment {
+    private final Name name;
+    private final MailAddress mailAddress;
+    private final Comment comment;
+
+    public static UserComment from(String name, String mailAddress, String comment) {
+        return new UserComment(
+                Name.from(name),
+                MailAddress.from(mailAddress),
+                Comment.from(comment)
+        );
+    }
+
+    /**
+     * 名前が[!omikuji]ならばおみくじの結果を返す.
+     * そうでないならばNameをそのまま返す.
+     *
+     * @return なまえ
+     */
+    public Name getName() {
+        if (!name.equals("!omikuji")) return name;
+
+        int random = new Random().nextInt(3);
+
+        switch (random) {
+            case 0:
+                return Name.from("大吉");
+            case 1:
+                return Name.from("中吉");
+            default:
+                return Name.from("小吉");
+        }
+    }
+}
