ab53c864563c3e3bd3c312ee86bd35285b55fb5b
toyodahiroki <toyo170920@gmail.com>
2021-05-16 13:42:53 +0900

    SELECTデータのDTO追加

---
+++ main/java/com/example/myapp/application/dto/UserCommentReadDto.java
@@ -0,0 +1,17 @@
+package com.example.myapp.application.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class UserCommentReadDto {
+    private final int id;
+    private final String name;
+    private final String mailAddress;
+    private final LocalDateTime createdAt;
+    private final String comment;
+}
